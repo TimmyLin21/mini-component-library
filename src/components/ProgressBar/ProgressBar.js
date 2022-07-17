25cc0941bd049e1d227e1f4431880820d083674f
TimmyLin21 <f7964032@gmail.com>
2022-07-17 12:19:36 +0800

    feature: add ProgressBar and Select

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,80 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  small: {
+    height: 8,
+    radius: 4,
+    padding: 0,
+  },
+  medium: {
+    height: 12,
+    radius: 4,
+    padding: 0,
+  },
+  large: {
+    height: 16,
+    radius: 8,
+    padding: 4,
+  },
+};
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  width: 100%;
+  border-radius: var(--radius);
+  padding: var(--padding);
+
+  // trim off the corner while progress near 100%
+  overflow: hidden;
+`;
+const Bar = styled.span`
+  display: block;
+  background-color: ${COLORS.primary};
+  width: var(--width);
+  height: var(--height);
+  border-radius: 4px 0 0 4px;
+`;
+
+const BarWrapper = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+`
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+  if (!styles) {
+    throw new Error(`The input size: ${size} is not exist!`);
+  }
+
+  return (
+    <Wrapper
+      style={{
+        "--radius": styles.radius + "px",
+        "--padding": styles.padding + "px",
+      }}
+    >
+      <span id="loadinglabel">
+        <VisuallyHidden>Loading:</VisuallyHidden>
+      </span>
+      <BarWrapper>
+        <Bar
+          role="progressbar"
+          aria-labelledby="loadinglabel"
+          aria-valuenow={value}
+          style={{
+            "--height": styles.height + 'px',
+            "--width": value + '%',
+          }}
+        ></Bar>
+      </BarWrapper>
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,64 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const SelectWrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+const SelectBase = styled.select`
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  /* Allow the select to span the full height in Safari */
+  -webkit-appearance: none;
+`
+const CustomSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 16px;
+  border-radius: 8px;
+  padding-right: 52px;
+
+  ${SelectBase}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color; 
+  }
+
+  ${SelectBase}:hover + & {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  width: var(--size);
+  height: var(--size);
+  margin: auto;
+  pointer-events: none;
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <SelectBase value={value} onChange={onChange}>
+        {children}
+      </SelectBase>
+      <CustomSelect>
+        {displayedValue}
+        <IconWrapper style={{'--size': 24 + 'px'}}>
+          <Icon id="chevron-down" strokeWidth={2} size={24}/>
+        </IconWrapper>
+      </CustomSelect>
+    </SelectWrapper>
   );
 };
 
